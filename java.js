9bce5d6df52fad9949b3115cc2a8d09cef0e0096
Németh Zalán B. <zalufox@gmail.com>
2024-12-12 14:16:09 +0100

    Az időzítőn dolgozom még, a html be beraktam span okat

--- java.js
+++ java.js
@@ -1,7 +1,58 @@
-let ido = document.getElementById("ido")
-let time = setInterval
+let bool = false
 
-function IndulAll () {
-    
+function play(){
+
+    if (bool == false) {
+        let time = setInterval(indit, 1000)
+        bool = true
+    }
+    else{
+        all()
+        bool = false
+    }
+
 
 }
+
+let perc = 1
+    let masperc = 0
+
+function indit(){
+console.log(1)
+
+    
+
+    if(document.getElementById("roma").innerHTML != "I" && perc == 0)
+    {
+        clearInterval(time)
+    }
+    else{
+
+        if (perc == 0) {
+            document.getElementById("roma").innerHTML = "II"
+            perc = 30
+        }
+        else if(masperc == 0){
+            perc -= 1
+            document.getElementById("perc").innerHTML = perc
+            masperc = 60
+        }
+        masperc -= 1
+        document.getElementById("masperc").innerHTML = masperc
+
+
+    }
+    
+    document.getElementById("perc").innerHTML = perc
+
+
+}
+
+function all(){
+    clearInterval(time)
+
+}
+
+//console.log(document.getElementById("roma").innerHTML)
+//console.log(document.getElementById("perc").innerHTML)
+//console.log(document.getElementById("masperc").innerHTML)
