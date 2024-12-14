a18aef3d37eb271ea13add135a6e31308472670d
Dominikzsupos <zsuposdominik@gmail.com>
2024-12-14 21:27:05 +0100

    kiegészítés és javítás

--- java.js
+++ java.js
@@ -1,5 +1,16 @@
 let bool = false
 
+//Az összes gomb disabled legyen a csapat véglegesítésen kivül amíg meg nem adja a csapat nevet VAGY amíg el nem indítja az időt, 
+//utána a csapat megadás eltünik VAGY csak disabled-et adsz neki, és a gombok enabledek,
+// de mivel te dolgozol az id-kel ezért írd át ha nem tetszenek, csak ideiglenesen raktam be 
+
+document.getElementById("kialitas1").disabled = true;
+document.getElementById("kialitas2").disabled = true;
+document.getElementById("pluszido").disabled = true;
+document.getElementById("plusz").disabled = true;
+document.getElementById("minusz").disabled = true;
+document.getElementById("plusz1").disabled = true;
+document.getElementById("minusz1").disabled = true;
 function play(){
 
     if (bool == false) {
